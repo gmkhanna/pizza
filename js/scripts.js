3e96cf71c57b566a2110657573440bf9ad298781
Grinil Khanna <gmkhanna@gmail.com>
2017-01-27 12:18:40 -0800

    Add special pizzas. Input desc in HTML. Add functionality to order and addresses.

--- js/scripts.js
+++ js/scripts.js
@@ -12,23 +12,33 @@ function Address(street, city, state) {
   this.state = state;
 }
 
-function Pizza(size, sauce, cheese, meat, veg, special1, special2) {
+function Pizza(price, size, sauce, cheese, meat, veg, special) {
+  this.price = price;
   this.size = size;
   this.sauce = sauce;
   this.cheese = cheese;
   this.meat = [];
   this.veg = [];
-  this.special1 = special1;
-  this.special2 = special2;
+  this.special = [];
 }
 
-var classico = new Pizza("Your Choice", "Marinara", "Mozzarella", "none", "Basil", "none", "none")
-var trailer = new Pizza("Your Choice", "Marinara", "Cheddar", "Pepperoni, Meatball", "none", "BBQ Chicken")
-var funkyFungus = new Pizza("Your Choice", "Garlic Fettuccine", "Mozzarella/Bleu", "none", "Mushroom", "Portobello", "Feta")
-var inflewenza = new Pizza("Your Choice", "Garlic Fettuccine", "Mozzarella", "Chicken", "Red Onions, Garlic", "none", "White Trufle")
-var skinnyChick = new Pizza("Your Choice", "Olive Oil", "Mozzarella/Bleu", "none", "Kale, Broccoli, Tofu", "none", "Avocado")
-var yellowFever = new Pizza("Your Choice", "Olive Oil", "Cheddar", "none", "Pineapple, Yellow Bell Pepper,", "none", "Thai Chicken")
 
+var classico = new Pizza("$15" "Large", "Marinara", "Mozzarella")
+classico.veg = ["Basil", "Tomato"]
+var trailer = new Pizza("$19", "Large", "Marinara", "Cheddar")
+trailer.meat = ["Pepperoni", "Meatball"]
+trailer.special = ["BBQ Chicken"]
+var funkyFungus = new Pizza("$17", "Large", "Garlic Fettuccine", "Mozzarella/Bleu", "Mushroom")
+funkyFungus.special = ["Portobello", "Feta"]
+var inflewenza = new Pizza("$16", "Large", "Garlic Fettuccine", "Mozzarella", "Chicken")
+inflewenza.veg = ["Red Onions", "Garlic"]
+inflewenza.special = ["White Trufle"]
+var skinnyChick = new Pizza("$19", "Large", "Olive Oil", "Mozzarella/Bleu")
+skinnyChick.veg = ["Kale," "Broccoli," "Tofu", "Lemon Zest"]
+skinnyChick.special = ["Avocado"]
+var yellowFever = new Pizza("$17", "Large", "Olive Oil", "Cheddar")
+yellowFever.veg = "Pineapple, Yellow Bell Pepper"
+yellowFever.special = ["Thai Chicken"]
 
 
 
