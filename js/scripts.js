db28ba69aef179cc9ef547a0fa70d2d0abffee4c
Grinil Khanna <gmkhanna@gmail.com>
2017-02-03 14:03:52 -0800

    Add clean slate - copied and pasted old scripts to other doc and starting from scratch to learn properly about constructors and prototypes

--- js/scripts.js
+++ js/scripts.js
@@ -1,143 +1,68 @@
 // Back-END
 
+function Pizza(size, sauce, cheese) {
+  this.size = size;
+  this.sauce = sauce;
+  this.cheese = cheese;
+};
 
-function Contact(first, last) {
-  this.firstName = first;
-  this.lastName = last;
-  this.location = [];
+Pizza.prototype.Desc = function() {
+  return this.size + this.sauce + this.cheese;
 }
 
-Contact.prototype.fullName = function() {
-  return this.firstName + " " + this.lastName;
-}
+
+
+function Name(first, last) {
+  this.first = first;
+  this.last = last;
+};
 
 function Address(street, city, state) {
   this.street = street;
   this.city = city;
   this.state = state;
+};
+
+function fullInfo(first, last, street, city, state) {
+  first = Name.first;
+  last = Name.last;
+  street = Address.street;
+  city = Address.city;
+  state = Address.state;
+  return Name.first + Name.last + Address.street + Address.city + Address.state;
 }
 
-// Address.prototype.fullAddress = function() {
-//   return ("<li>" + this.street + "</li>" + "<li>" + this.city + "</li>" + "<li>" + this.state + "</li>");
-// }
 
-// Pizza.meatOpt.prototype.meatOpt = function() {
-//   return $(".meat input:checked").each(function() {
-//     newPizza.meatOpt.push($(this).val());
-//   });
-// }
+// Front End
 
-var price;
-
-  // this.size = size;
-  // this.sauce = sauce;
-  // this.cheese = cheese;
-  // this.meatOpt = [];
-  // this.vegOpt = [];
-  // this.specialOpt = [];
-  // return ("<li> Size: " + this.size + "</li>" + "<li> Sauce: " + this.sauce + "</li>" + "<li> Cheese: " + this.cheese + "</li>" + "<li> Meat: " + this.meatOpt + "</li>" + "<li> Veggies: " + this.vegOpt + "</li>" + "<li> Special Toppings: " + this.specialOpt + "</li>")
-
-  Pizza.prototype.SizePrice = function(size, meatOpt, vegOpt, specialOpt) {
-    var basePrice = 0;
-    if (this.size === "small") {
-    var basePrice = 5
-    } else if (this.size === "medium") {
-    var basePrice = 10
-    } else {
-    var basePrice = 15
-    }
-  }
-
-  Pizza.prototype.AddToppings = function(meatOpt, vegOpt, SpecialOpt) {
-    var meatTopPrice;
-    for (var i = 0; i < pizza.meatOpt.length-1; i++){
-      meatTopPrice = pizza.meatOpt[pizza.meatOpt.length-1] * 1;
-    }
-    console.log(Pizza.AddToppings);
-  }
-
-
-function Pizza(size, sauce, cheese) {
-  this.size = size;
-  this.sauce = sauce;
-  this.cheese = cheese;
-  this.meatOpt = [];
-  this.vegOpt = [];
-  this.specialOpt = [];
-}
-
-Pizza.meat = [" Pepperoni", " Chicken", " Meatball"]
-Pizza.vegOpt = [" Broccoli", " Pineapple", " Red Onions", " Yellow Bell Pepper", " Tofu", " Kale", " Mushrooms", " Lemon Zest"];
-Pizza.specialOpt = [" Portobello", " Feta", " Prosciutto", " BBQ Chicken", " Thai Chicken", " White Truffle", " Avocado"]
-
-var classico = new Pizza(15, "Large", "Marinara", "Mozzarella")
-classico.veg = ["Basil", "Tomato"]
-var tailgate = new Pizza(19, "Large", "Marinara", "Cheddar")
-tailgate.meat = ["Pepperoni", "Meatball,"]
-tailgate.special = ["BBQ Chicken"]
-var funkyFungus = new Pizza(17, "Large", "Garlic Fettuccine", "Mozzarella/Bleu", "Mushroom")
-funkyFungus.special = ["Portobello", "Feta"]
-var inflewenza = new Pizza(19, "Large", "Garlic Fettuccine", "Mozzarella", "Chicken")
-inflewenza.veg = ["Red Onions", "Garlic"]
-inflewenza.special = ["White Trufle"]
-var skinnyChick = new Pizza(18, "Large", "Olive Oil", "Mozzarella/Bleu")
-skinnyChick.veg = ["Kale", "Broccoli", "Tofu", "Lemon Zest"]
-skinnyChick.special = ["Avocado"]
-var yellowFever = new Pizza(17, "Large", "Olive Oil", "Cheddar")
-yellowFever.veg = "Pineapple, Yellow Bell Pepper"
-yellowFever.special = ["Thai Chicken"]
-
-
-// Front-END
 $(document).ready(function() {
-
-  function AddressOut(x,y,z) {
-    $('#colConfirmAddress').text(Address.street);
-    $('#colConfirmAddress').text(Address.city);
-    $('#colConfirmAddress').text(Address.state);
-  }
 
   $("form#mainOrderForm").submit(function(event) {
     event.preventDefault();
 
-    var inputtedFirstName = $("#new-first-name").val();
-    var inputtedLastName = $("#new-last-name").val();
-    var newContact = new Contact(inputtedFirstName, inputtedLastName);
+//Pizza Area order value info
 
-    var inputtedStreet = $("#new-street").val();
-    var inputtedCity = $("#new-city").val();
-    var inputtedState = $("#new-state").val();
-    var newAddress = new Address(inputtedStreet, inputtedCity, inputtedState)
+var testPizza = new Pizza("large", "mar", "moz")
+console.log(testPizza.Desc());
+
+$(".pizzaConstruction").text(testPizza.Desc());
 
 
-    var inputtedSizePrice = parseInt($("#pizzaSize").val());
-    var inputtedSauce = $("#pizzaSauce").val();
-    var inputtedCheese = $("#pizzaCheese").val();
-    var checkedMeats = $('input:checkbox:checked.meat').map(function() {
-      return this.value;
-    }).get();
-
-    var checkedVeg = $('input:checkbox:checked.veg').map(function() {
-      return this.value;
-    }).get();
-
-    var checkedSpecial = $('input:checkbox:checked.special').map(function() {
-      return this.value;
-    }).get();
-
-    var newPizza = new Pizza(inputtedSizePrice, inputtedSauce, inputtedCheese, checkedMeats, checkedVeg, checkedSpecial)
-
-    // $(".pizzaConstruction").append(newPizza.PizzaOrder());
-    $(".confirmFullName").text(newContact.fullName());
-    // $(".confirmAddress").append(newAddress.fullAddress());
-     (AddressOut(newAddress));
 
 
-      $("#new-first-name").val("");
-      $("#new-last-name").val("");
 
 
-    // });
+//Name Area input info
+  // var first = $("#new-first-name").val();
+  // var last = $("#new-last-name").val();
+  // var street = $("#new-street").val();
+  // var city = $("#new-city").val();
+  // var state = $("#new-state").val();
+  //
+  //   $(".confirmFullName").text(fullInfo());
+  //   console.log(fullInfo());
+
+
 
   });
 });
