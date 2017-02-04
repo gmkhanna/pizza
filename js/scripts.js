192fe3f13f129ca26226730b6cc03fd77174d8ab
Grinil Khanna <gmkhanna@gmail.com>
2017-02-03 16:46:47 -0800

    have sizePrice prototype function working and displaying in total area. Now work on for loop with toppings and arrays

--- js/scripts.js
+++ js/scripts.js
@@ -11,16 +11,15 @@ Pizza.prototype.Desc = function() {
   return this.size + this.sauce + this.cheese;
 }
 
-Pizza.prototype.SizePrice = function() {
-  var basePrice = 1;
-  if (sizeChoice === "small") {
-    return basePrice * 5;
-  } else if (sizeChoice === "medium") {
-    return basePrice * 10
+Pizza.prototype.SizePrice = function(sizeChoice) {
+  var basePrice;
+  if (this.size === "Small") {
+    return basePrice = 5;
+  } else if (this.size === "Medium") {
+    return basePrice = 10;
   } else {
-    return basePrice * 15
+    return basePrice = 15;
   }
-  console.log(newOrder.SizePrice());
 }
 
 
@@ -52,36 +51,37 @@ $(document).ready(function() {
   $("form#mainOrderForm").submit(function(event) {
     event.preventDefault();
 
-//Pizza Area order value info
+    //Pizza Area order value info
 
-//This is just to test
-// var testPizza = new Pizza("large", "mar", "moz")
-//
-// $("#size").text(testPizza.size);
-// $("#sauce").text(testPizza.sauce);
-// $("#cheese").text(testPizza.cheese);
+    //This is just to test
+    // var testPizza = new Pizza("large", "mar", "moz")
+    //
+    // $("#size").text(testPizza.size);
+    // $("#sauce").text(testPizza.sauce);
+    // $("#cheese").text(testPizza.cheese);
 
-var sizeChoice = $("#newSize").val();
-var sauceChoice = $("#newSauce").val();
-var cheeseChoice = $("#newCheese").val();
-var newOrder = new Pizza(sizeChoice, sauceChoice, cheeseChoice);
-
-
-$("#size").text(sizeChoice);
-$("#sauce").text(sauceChoice);
-$("#cheese").text(cheeseChoice);
-console.log(newOrder.Desc());
+    var sizeChoice = $("#newSize").val();
+    var sauceChoice = $("#newSauce").val();
+    var cheeseChoice = $("#newCheese").val();
+    var newOrder = new Pizza(sizeChoice, sauceChoice, cheeseChoice);
 
 
-//Name Area input info
-  // var first = $("#new-first-name").val();
-  // var last = $("#new-last-name").val();
-  // var street = $("#new-street").val();
-  // var city = $("#new-city").val();
-  // var state = $("#new-state").val();
-  //
-  //   $(".confirmFullName").text(fullInfo());
-  //   console.log(fullInfo());
+    $("#size").text(sizeChoice);
+    $("#sauce").text(sauceChoice);
+    $("#cheese").text(cheeseChoice);
+
+    $("#totalPrice").text(newOrder.SizePrice());
+
+
+    //Name Area input info
+    // var first = $("#new-first-name").val();
+    // var last = $("#new-last-name").val();
+    // var street = $("#new-street").val();
+    // var city = $("#new-city").val();
+    // var state = $("#new-state").val();
+    //
+    //   $(".confirmFullName").text(fullInfo());
+    //   console.log(fullInfo());
 
 
 
